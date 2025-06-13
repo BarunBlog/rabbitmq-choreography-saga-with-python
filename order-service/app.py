f5e5ef5a716baafe4851a3f3fd4538a5f8487842
BarunBlog <bhattacharjeebarun25@gmail.com>
2025-06-13 13:26:31 +0600

    Completed order service and send messages the rabbitmq

---
+++ order-service/app.py
@@ -0,0 +1,34 @@
+import json
+import uuid
+from quart import Quart, jsonify
+from rabbitmq import publish_message
+
+app = Quart(__name__)
+
+@app.route("/order", methods=["POST"])
+async def create_order():
+
+    exchange = "order_exchange"
+    routing_key = "order.created"
+
+    try:
+        print("[Order] Started processing the order", flush=True)
+
+        # Simulating basic order data
+        order = {
+            "order_uuid": str(uuid.uuid4()),
+            "status": "created"
+        }
+
+        # Publish order data to the exchange
+        await publish_message(exchange=exchange, routing_key=routing_key, message=json.dumps(order))
+
+        return jsonify({"message": "Order created and event published", "order": order}), 201
+
+    except Exception as e:
+        print(f"[ERROR] Failed to create the order: {e}", flush=True)
+        return jsonify({"message": "Failed to create the order"}), 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4001)
---
+++ order-service/rabbitmq.py
@@ -0,0 +1,39 @@
+import aio_pika
+import os
+
+_connection = None
+_channel = None
+
+async def connect_rabbit():
+    global _connection, _channel
+
+    if _connection is None or _connection.is_closed:
+        print("[RabbitMQ] Connecting to RabbitMQ...", flush=True)
+
+        try:
+            _connection = await aio_pika.connect_robust(os.environ.get("RABBITMQ_URL"))
+            _channel = await _connection.channel()
+
+            print("[RabbitMQ] Connected to RabbitMQ...", flush=True)
+        except Exception as e:
+            print(f"[ERROR] Failed to connect to RabbitMQ: {e}", flush=True)
+
+    return _channel
+
+
+async def publish_message(exchange: str, routing_key: str, message: str):
+    channel = await connect_rabbit()
+
+    try:
+        # Declare or create the exchange if needed.
+        exchange_obj = await channel.declare_exchange(name=exchange, type=aio_pika.ExchangeType.DIRECT, durable=True)
+
+        # Publish the message to the exchange with the routing key
+        await exchange_obj.publish(
+            message=aio_pika.Message(body=message.encode(), delivery_mode=aio_pika.DeliveryMode.PERSISTENT),
+            routing_key=routing_key
+        )
+
+        print(f"Message published to exchange '{exchange}' with routing key '{routing_key}': {message}", flush=True)
+    except Exception as e:
+        print(f"[ERROR] Failed to publish message: {e}", flush=True)
